3d8e44fdb14b21e558f467fc997fe72a7e308862
Nejintev Nicolai <niksaysit@gmail.com>
2023-10-02 11:58:56 +0300

    Add university class

---
+++ src/main/java/lab1/University.java
@@ -0,0 +1,11 @@
+package lab1;
+
+import java.util.HashMap;
+
+public class University {
+    private HashMap<String, Faculty> faculties;
+
+    public University() {
+        this.faculties = new HashMap<String, Faculty>();
+    }
+}
