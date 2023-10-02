2d3a9cb5db4de7faebaa2cbc8101eeb62d6c79be
Nejintev Nicolai <niksaysit@gmail.com>
2023-10-02 12:48:59 +0300

    Add basic faculty and student methods

--- src/main/java/lab1/Faculty.java
+++ src/main/java/lab1/Faculty.java
@@ -1,6 +1,7 @@
 package lab1;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 public class Faculty {
     private String name;
@@ -14,5 +15,46 @@ public class Faculty {
         this.studyField = studyField;
 
         this.students = new ArrayList<Student>();
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getAbbreviation(){
+        return this.abbreviation;
+    }
+
+    public StudyField getStudyField() {
+        return this.studyField;
+    }
+
+    public void addStudent(Student student) {
+        // TODO check existing email
+        this.students.add(student);
+    }
+
+    public Optional<Student> findStudent(String email) {
+        return this.students.stream().
+            filter(student -> student.getEmail().equals(email)).
+            findFirst();
+    }
+
+    public ArrayList<Student> getStudents(boolean graduated) {
+        ArrayList<Student> result = new ArrayList<Student>();
+        if (graduated) {
+            this.students.stream().
+                filter(student -> student.isGraduated()).
+                forEach(student -> result.add(student));
+        } else {
+            this.students.stream().
+                filter(student -> !student.isGraduated()).
+                forEach(student -> result.add(student));
+        }
+        return result;
+    }
+
+    public boolean isStudentHere(String email) {
+        return this.findStudent(email).isPresent();
     }
 }
--- src/main/java/lab1/Student.java
+++ src/main/java/lab1/Student.java
@@ -8,13 +8,39 @@ public class Student {
     private String email;
     private Date   enrollmentDate;
     private Date   dateOfBirth;
+    private boolean graduated;
 
     public Student(String firstName, String lastName, String email, Date dateOfBirth) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
         this.dateOfBirth = dateOfBirth;
-
+        
         this.enrollmentDate = new Date();
+        this.graduated = false;
+    }
+
+    public String getName() {
+        return this.lastName + this.firstName;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public Date getEnrollmentDate() {
+        return this.enrollmentDate;
+    }
+
+    public Date getDateOfBirth() {
+        return this.dateOfBirth;
+    }
+
+    public boolean isGraduated() {
+        return this.graduated;
+    }
+
+    public void graduate() {
+        this.graduated = true;
     }
 }
