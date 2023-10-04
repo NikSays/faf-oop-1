7a39f15f8a5b739cf88b1fceb7356ae713ad1e29
Nejintev Nicolai <niksaysit@gmail.com>
2023-10-04 10:16:57 +0300

    Run the shell

--- src/main/java/lab1/Main.java
+++ src/main/java/lab1/Main.java
@@ -1,15 +1,25 @@
 package lab1;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 import lab1.log.Level;
 import lab1.log.TXTLogger;
+import lab1.session.BinaryFileManager;
+import lab1.shell.UniversityMenu;
+import lab1.universityStructure.University;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        // Initialize dependencies
         TXTLogger.init("./log.txt", Level.DEBUG);
+        BinaryFileManager stateManager = new BinaryFileManager("./.session");
 
+        UniversityMenu shell = new UniversityMenu(
+            new Scanner(System.in),
+            new University(stateManager)
+        );
 
-
+        shell.run();
     }
 }
--- src/main/java/lab1/shell/Shell.java
+++
@@ -1,7 +0,0 @@
-package lab1.shell;
-
-public class Shell {
-    public static void run() {
-        System.out.println("not implemented");
-    }
-}
